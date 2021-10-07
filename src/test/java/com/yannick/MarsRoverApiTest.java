a5b2e37b927f6398eb8f5083c6a89a640bba9d11
irAuditor <34457996+irAuditor@users.noreply.github.com>
2021-10-07 14:53:27 +0200

    changed app to postgresql

---
+++ src/test/java/com/yannick/MarsRoverApiTest.java
@@ -0,0 +1,19 @@
+package com.yannick;
+
+
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.yannick.response.MarsRoverApiResponse;
+
+public class MarsRoverApiTest {
+
+  @Test
+  public void smallTest () {
+    RestTemplate rt = new RestTemplate();
+    
+    ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=DEMO_KEY", MarsRoverApiResponse.class);
+    System.out.println(response.getBody());
+  }
+}
